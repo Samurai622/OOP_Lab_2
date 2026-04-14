90cf3d066cb02be08c348438b22105ae34d46834
Janlast <foxyvdov@gmail.com>
2026-04-14 13:24:36 +0300

    Add VolumeConverterService for unit conversion of liquid volumes
    - Implemented a service to convert between various volume units including milliliters, liters, cubic meters, and more.
    - Added a dictionary to store conversion rates relative to liters.
    - Provided methods to retrieve available units and perform conversions.

---
+++ Calculator/Services/VolumeConverterService.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Calculator.Services;
+
+
+public class VolumeConverterService
+{
+   // Усі значення вказані відносно 1 Літра
+   private readonly Dictionary<string, double> _conversionRates = new()
+   {
+       { "Мілілітри (ml)", 0.001 },
+       { "Кубічні сантиметри (cm³)", 0.001 },
+       { "Літри (L)", 1.0 },
+       { "Кубічні метри (m³)", 1000.0 },
+       { "Чайні ложки (tsp)", 0.00492892 },
+       { "Столові ложки (tbsp)", 0.0147868 },
+       { "Рідкі унції (fl oz)", 0.0295735 },
+       { "Чашки (cup)", 0.236588 },
+       { "Пінти (pt)", 0.473176 },
+       { "Галони (gal)", 3.78541 }
+   };
+
+
+   public List<string> GetUnits() => _conversionRates.Keys.ToList();
+
+
+   public double Convert(double amount, string fromUnit, string toUnit)
+   {
+       if (!_conversionRates.ContainsKey(fromUnit) || !_conversionRates.ContainsKey(toUnit))
+           return 0;
+
+
+       // Переводимо у літри
+       double inLiters = amount * _conversionRates[fromUnit];
+      
+       // З літрів у цільову одиницю
+       return inLiters / _conversionRates[toUnit];
+   }
+}
+
+
+
